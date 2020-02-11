865fce56682115cc099dacddf0cd8646dc9d1996
Shabab Ayub <s4ayub@edu.uwaterloo.ca>
2020-02-11 16:25:01 -0500

    global vars are weird in python

---
+++ find_correct_combination_of_k_length_given_alphabet.py
@@ -0,0 +1,24 @@
+global_mon = ""
+global_bool = False
+
+def is_valid(secret, guess):
+    return secret == guess
+
+def gen_guesses(set, guess, guess_len, alpha_len, secret_len, secret):
+    global global_mon
+    global global_bool
+
+    if is_valid(secret, guess):
+        global_mon = guess
+        global_bool = True
+    else:
+        for i in range(alpha_len):
+            if not global_bool and guess_len <= secret_len:
+                new_guess = guess + set[i]
+                gen_guesses(set, new_guess, guess_len + 1, alpha_len, secret_len, secret)
+            else:
+                break
+    return
+
+gen_guesses(["a", "b", "c"], "", 0, 3, 2, "ab")
+print(global_mon)
